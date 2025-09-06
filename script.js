48f77d7bda2d9000b820f1457920a5bc44cda9e9
ayanC231209 <c231209@ugrad.iiuc.ac.bd>
2025-09-07 03:32:24 +0600

    toggle done

--- script.js
+++ script.js
@@ -10,3 +10,66 @@ list.addEventListener("click",function(e){
 
 })
 
+let currentfilter="all"
+
+
+function display(){
+
+  const status =document.querySelectorAll(".card")
+
+  status.forEach(e =>{
+
+    const s=e.dataset.status
+   if(currentfilter==="all"){
+ e.classList.remove("hidden")
+
+   }
+   else{
+
+if(s === currentfilter){
+  e.classList.remove("hidden")
+}
+else{
+
+  e.classList.add("hidden")
+}
+
+   }
+
+
+  })
+
+
+
+}
+
+
+
+
+document.querySelectorAll(".choose").forEach(e =>{
+
+e.addEventListener("change",()=>{
+
+  currentfilter=e.dataset.target
+  display()
+
+})
+
+})
+
+
+
+document.querySelectorAll(".toggle").forEach(e =>{
+
+e.addEventListener("change",()=>{
+
+const cards=e.closest(".card")
+
+cards.dataset.status=e.checked?"active" : "inactive"
+display()
+
+})
+
+})
+
+
